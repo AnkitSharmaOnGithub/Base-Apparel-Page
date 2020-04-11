a9717752efdd07f4c764ea8ab069996c0d0cfad6
unknown <ankitsharma101299@gmail.com>
2020-04-11 20:28:21 +0530

    Updated validation logic.

--- js/app.js
+++ js/app.js
@@ -6,15 +6,23 @@ let status = document.querySelector('.status');
 form.addEventListener('submit',e => {
     e.preventDefault();
     if(validate_mail(email)){
-        
+        proceed.classList.remove('error');
+        proceed.classList.add('success');
+        setMessage('Thank you! Check your email to confirm your email subscription.');
     }
     else{
-
+        proceed.classList.remove('success');
+        proceed.classList.add('error');
+        setMessage('Please enter valid email.');
     }
 
 })
 
-validate_mail(){
+function validate_mail(){
     var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test()
+}
+
+function setMessage(value){
+    status.innerText = value;
 }
